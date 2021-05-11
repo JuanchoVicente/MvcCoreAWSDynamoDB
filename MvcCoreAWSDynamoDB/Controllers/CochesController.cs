97978d4ce623ebb62157712bbdaaa1fa8377db8b
Juan Carlos Vicente Vicente <JuanCarlos.Vicente@tajamar365.com>
2021-05-11 10:22:56 +0200

    Configurado y funcional con DynamoDB

---
+++ MvcCoreAWSDynamoDB/Controllers/CochesController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAWSDynamoDB.Models;
+using MvcCoreAWSDynamoDB.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreAWSDynamoDB.Controllers
+{
+    public class CochesController : Controller
+    {
+
+        ServiceAWSDynamoDb service;
+
+        public CochesController(ServiceAWSDynamoDb service)
+        {
+            this.service = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ListCoches()
+        {
+            return View(await this.service.GetCochesAsync());
+        }
+
+        public async Task<IActionResult> DetailsCoche(int id)
+        {
+            return View(await this.service.GetCocheAsync(id));
+        }
+
+        public IActionResult CreateCar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar(Coche car
+            , String incluirmotor, String tipo
+            , int caballos, int cilindrada)
+        {
+            if (incluirmotor != null)
+            {
+                car.Motor = new Motor();
+                car.Motor.Tipo = tipo;
+                car.Motor.Caballos = caballos;
+                car.Motor.Cilindrada = cilindrada;
+            }
+
+            await this.service.CrearCoche(car);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteCar(int idCoche)
+        {
+            await this.service.DeleteCocheAsync(idCoche);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreAWSDynamoDB/Models/Coche.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreAWSDynamoDB.Models
+{        
+[DynamoDBTable("coches")]
+    public class Coche
+    {
+        [DynamoDBProperty("idcoche")]
+        [DynamoDBHashKey]
+        public int IdCoche { get; set; }
+            
+        [DynamoDBProperty("marca")]
+        public String Marca { get; set; }
+            
+        [DynamoDBProperty("modelo")]
+        public String Modelo { get; set; }
+            
+        [DynamoDBProperty("velocidadmaxima")]
+        public int VelocidadMaxima { get; set; }
+
+        [DynamoDBProperty("imagen")]
+        public String Imagen { get; set; }
+
+        [DynamoDBProperty("motor")]
+        public Motor Motor { get; set; }
+
+
+        
+    }   
+}
